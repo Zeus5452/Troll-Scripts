4020090261fff2e90d531376e9bc60c8d0c5e89f
Feesh <74217010+Goldfeesh@users.noreply.github.com>
2020-11-23 20:59:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import os, pyautogui, time
 
 arr = os.listdir('spamFiles')
-print(arr)
 
-y = input('Choose File: \n')
+print("Available files:\n")
+
+for i in arr:
+    print(f"- {i}")
+
+while True:
+    y = input('\nChoose File: \n')
+
+
+
+    if y not in arr:
+        if y == "files":
+            for i in arr:
+                print(f"- {i}")
+
+
+        if y != "files":
+            print("Sorry, this file does not exist!\nType 'files' to see all available files.")
+        continue
+    break
+
 f = open(f"spamFiles\\{y}", 'r')
-time.sleep(5)
+
+print(f"\nYou opened: {y}!\nBe sure to focus on the window of your choice.\nStarting in:")
+
+x = 5
+for i in range(0, x):
+    time.sleep(1)
+    print("\r", end='')
+    print("%i" % int(x - i), end='', flush=True)
+
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press('enter')
