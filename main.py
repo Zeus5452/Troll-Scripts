8244de23e5ecb51324fd7d5be369e453d8a88f1d
Feesh <74217010+Goldfeesh@users.noreply.github.com>
2020-11-24 02:42:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,76 @@
 import os, pyautogui, time
 
-# OPEN FILE
 
-arr = os.listdir('spamFiles')
+def countdown(arg):
+    for i in range(0, arg):
+        print("\r", end='')
+        print("%i" % int(arg - i), end='', flush=True)
+        time.sleep(1)
 
-print("Available files:\n")
-
-for i in arr:
-    print(f"- {i}")
-
-while True:
-    y = input('\nChoose File: ')
-
-    if y not in arr:
-        if y == "files":
-            for i in arr:
-                print(f"- {i}")
-
-        if y != "files":
-            print("Sorry, this file does not exist!\nType 'files' to see all available files.")
-        continue
-    break
-
-# CHOOSE TIME
 
 while True:
-    timer = input("\nHow long do you want the time in between messages to be (sec)?\nChoose a number: ")
+    # OPEN FILE
 
-    try:
-        float(timer)
-        if float(timer) < -0.1:
-            print("This is not a valid number!")
+    arr = os.listdir('spamFiles')
+
+    print("Available files:\n")
+
+    for i in arr:
+        print(f"- {i}")
+
+    while True:
+        y = input('\nChoose File: ')
+
+        if y not in arr:
+            if y == "files":
+                for i in arr:
+                    print(f"- {i}")
+
+            if y != "files":
+                print("Sorry, this file does not exist!\nType 'files' to see all available files.")
             continue
         break
 
-    except ValueError:
-        print("This is not a valid number!")
+    # CHOOSE TIME
 
-# OPENING
+    while True:
+        timer = input("\nHow long do you want the time in between messages to be (sec)?\nChoose a number: ")
 
-f = open(f"spamFiles\\{y}", 'r')
+        try:
+            float(timer)
+            if float(timer) < -0.1:
+                print("This is not a valid number!")
+                continue
+            break
 
-print(
-    f"\nYou opened: {y} with a timer of {timer} seconds!\nBe sure to focus on the window of your choice.\nStarting in:")
+        except ValueError:
+            print("This is not a valid number!")
 
-x = 5
-for i in range(0, x):
-    time.sleep(1)
+    # OPENING
+
+    f = open(f"spamFiles\\{y}", 'r')
+
+    print(f"\nYou opened: {y} with a timer of {timer} seconds!\nBe sure to focus on the window of your choice.\nStarting in:")
+
+    countdown(5)
     print("\r", end='')
-    print("%i" % int(x - i), end='', flush=True)
+    print("Started!", end='', flush=True)
 
-print("\r", end='')
-print("Started!\n", end='', flush=True)
+    # SCRIPT
 
-# SCRIPT
+    float(timer)
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press('enter')
+        time.sleep(float(timer))
 
-float(timer)
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press('enter')
-    time.sleep(float(timer))
+    print("\r", end='')
+    print("Done!", end='', flush=True)
 
-print("\r", end='')
-print("Done!\n", end='', flush=True)
+    print("\n\nPress enter to restart")
+    z = input()
+    print("Restarting in:")
+
+    countdown(15)
+    print("\n" * 100)
+
