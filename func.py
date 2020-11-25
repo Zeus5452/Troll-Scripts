d37e7c53d4eb52693c2aaac565ddf04a4dc05c13
Feesh <74217010+Goldfeesh@users.noreply.github.com>
2020-11-25 18:20:27 +0100

    Wikispam :)

--- func.py
+++ func.py
@@ -1,5 +1,12 @@
 import os, pyautogui, time, random, string
-
+import wikipedia, random, pyautogui,time
+import re
+alphabets = "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
 
 def countdown(arg):
     for i in range(0, arg):
@@ -114,10 +121,83 @@ def randomstring():
     print("Done!", end='', flush=True)
 
 
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text: text = text.replace(".”", "”.")
+    if "\"" in text: text = text.replace(".\"", "\".")
+    if "!" in text: text = text.replace("!\"", "\"!")
+    if "?" in text: text = text.replace("?\"", "\"?")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+
+def wikispam():
+
+    while True:
+        timer = input("\nHow long do you want the time in between messages to be (sec)?\nChoose a number: ")
+
+        try:
+            float(timer)
+            if float(timer) < -0.1:
+                print("This is not a valid number!")
+                continue
+            break
+
+        except ValueError:
+            print("This is not a valid number!")
+
+    print(
+        f"\nYou opened a random wiki article with a timer of {timer} seconds!\nBe sure to focus the window of your "
+        f"choice.\nStarting in:")
+
+    try:
+        wikipage = wikipedia.random(1)
+        wikiload = wikipedia.page(wikipage).content
+
+        page = split_into_sentences(wikiload)
+
+        countdown(5)
+
+        print("\r", end='')
+        print("Started!", end='', flush=True)
+
+        float(timer)
+        for word in page:
+            pyautogui.typewrite(word)
+            pyautogui.press('enter')
+            time.sleep(float(timer))
+
+        print("\r", end='')
+        print("Done!", end='', flush=True)
+
+
+
+    except wikipedia.DisambiguationError as e:
+        s = random.choice(e.options)
+        p = wikipedia.pages(s)
+
+
 def restart():
     print("\n\nPress enter to restart")
     z = input()
     print("Restarting in:")
 
-    countdown(15)
+    countdown(5)
     print("\n" * 100)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ while True:
     print("Pick your spam tool here!\npick from:")
     print("1. Filespam - Spams everything word for word in a certain txt file")
     print("2. Random string spam - Spams a random string")
+    print("3. Wikispam - Spams a random wikipedia article")
 
     f = input("\nType the corresponding number to the option you want to pick\n> ")
 
@@ -18,6 +19,11 @@ while True:
     if f == str(2):
         print("\nYou picked: Random string!\n\n")
         randomstring()
+        restart()
+
+    if f == str(3):
+        print("\nYou picked: Wikispam!\n\n")
+        wikispam()
         restart()
 
 
